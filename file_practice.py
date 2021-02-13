5cabb498d2ddb7684088bd30e79c542af2498fc9
Xiloid <faustov.roman.a@gmail.com>
2021-02-13 20:31:42 +0200

    comm

--- file_practice.py
+++ file_practice.py
@@ -4,13 +4,26 @@
     Записать в него строку 'Starting practice with files'
     Файл должен заканчиваться пустой строкой
 """
+import re
 
+
+from pathlib import Path
+'''path = Path('files/file_practice.txt').resolve()
+with open(path, 'w') as f:
+    f.write('Starting practice with files')
+    f.close()
 """
     2.
     Прочесть первые 5 символов файла и вывести содержимое в верхнем регистре
     Затем прочесть весь файл от начала до конца, вывести содержимое на экран
 """
-
+with open(path, 'r') as f:
+    f.seek(0)
+    file_symbols = f.read(5)
+    print(str.upper(file_symbols))
+    f.seek(0)
+    print(f.read())
+    f.close()'''
 """
     3.
     Прочесть файл files/text.txt
@@ -18,7 +31,19 @@
     иначе - заменить все буквы 'e' на 'i'
     Полученный текст дописать в файл file_practice.txt
 """
-
+path2 = Path('files/text.txt').resolve()
+with open(path2, 'r+') as f:
+    f.seek(0)
+    all_text = f.read()
+    i = all_text.count('i')
+    e = all_text.count('e')
+    if i > e:
+        all_text = re.sub('e', 'i', all_text)
+    else:
+        all_text = re.sub('i', 'e', all_text)
+    f.seek(0, 2)
+    f.write(all_text)
+    f.close()
 """
     4.
     Если в файле file_practice.txt четное количество элементов
